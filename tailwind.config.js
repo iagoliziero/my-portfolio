75657302a47602876744033a6a4654b7934fe684
iagoliziero <iagolizierox14@gmail.com>
2025-07-04 12:39:21 -0300

    chore: configuring tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      'md': '640px',
+      'mil': '960px',
+      'lg': '1024px',
+      'xl': '1281px',
+      'xxl': '1470px',
+       '3xl': '1820px',
+    },
     extend: {},
   },
   plugins: [],

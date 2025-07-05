f94ee85c264c26581cfeafcbc661bc28922700a5
iagoliziero <iagolizierox14@gmail.com>
2025-07-05 15:10:25 -0300

    feat: create HeroButton component

---
+++ src/components/HeroButton.tsx
@@ -0,0 +1,14 @@
+const HeroButton = (): JSX.Element => {
+    return ( 
+        <div className="flex flex-col md:flex-row items-center justify-center gap-5">
+        <button className="p-5 w-52 text-[18px] bg-gradient-to-r from-purple-600 to-pink-600 rounded-full text-white font-semibold shadow-lg hover:bg-purple-600 transform transition hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none hover:from-purple-700 hover:to-pink-700">
+          Ver Meus Projetos
+        </button>
+        <button className="p-5 w-52 text-[18px] rounded-full border-2 border-purple-400 text-purple-400 font-semibold shadow-lgtransition  transform transition hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none hover:bg-purple-400 hover:text-white">
+          Vamos Conversar
+        </button>
+      </div>
+     );
+}
+ 
+export default HeroButton;

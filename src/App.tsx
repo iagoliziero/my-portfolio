b8880ac186dd545a71d95e25f05c0c3aa7f80cf2
iagoliziero <iagolizierox14@gmail.com>
2025-07-05 16:57:00 -0300

    fix: adjusting errors of layout

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ import SocialsSection from "./components/SocialsSection";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-slate-950 via-gray-950 to-slate-900 dark:from-black dark:via-gray-900 dark:to-black transition-all duration-300 overflow-x-hidden">
+    <div className="min-h-screen bg-gradient-to-br from-slate-950 via-gray-950 to-slate-900 dark:from-black dark:via-gray-900 dark:to-black transition-all duration-300 overflow-x-hidden ">
       <Header />
       {/* Hero Section */}
-      <section className="relative flex flex-col items-center justify-center mt-60 lg:mt-64 xl:mt-72">
+      <section className="relative flex flex-col items-center justify-center  top-60 lg:top-72">
        <HeroContent />
         <div className="flex flex-col items-center justify-center">
           {" "}
@@ -21,10 +21,14 @@ function App() {
       <SocialsSection />
       {/* Socials section */}
       {/* arrow down */}
-      <div className="relative top-5 text-white/70 left-1/2 transform -translate-x-1/2 animate-bounce mb-64">
+      <div className="relative top-60 lg:top-72 text-white/70 left-1/2 transform -translate-x-1/2 animate-bounce">
       <ChevronDown width={35} height={35} />
       </div>
+      <div className="max-h-0 relative top-[25rem]">
+
+      </div>
       {/* arrow down */}
+     
     </div>
     
   );

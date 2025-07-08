92fb3ecb4d80069f4e40f1fbd22418534b925d27
iagoliziero <iagolizierox14@gmail.com>
2025-07-08 15:16:58 -0300

    feat: adding two logotypes in MyMarquee

--- src/components/MyMarquee.tsx
+++ src/components/MyMarquee.tsx
@@ -9,14 +9,13 @@ const MyMarquee = () => (
     pauseOnHover={true}
     className=" m-5 overflow-hidden w-full bg-white/5 p-6 rounded-xl border-slate-300/30 border hover:bg-white/10 transition-all ease-in-out duration-300"
   >
-    <div className="flex gap-5">
+    <div className="flex gap-5 items-center">
       <br />
       <ImgMarque nameImg="nodedotjs" />
       <ImgMarque nameImg="express" />
       <ImgMarque nameImg="javascript" />
       <ImgMarque nameImg="html5" />
       <ImgMarque nameImg="css" />
-      <ImgMarque nameImg="git" />
       <ImgMarque nameImg="react" />
       <ImgMarque nameImg="vite" />
       <ImgMarque nameImg="tailwindcss" />
@@ -24,9 +23,14 @@ const MyMarquee = () => (
       <ImgMarque nameImg="mysql" />
       <ImgMarque nameImg="mongodb" />
       <ImgMarque nameImg="figma" />
-      <ImgMarque nameImg="nodedotjs" />
       <ImgMarque nameImg="git" />
+
+      {/* logotypes that don`t exist in simpleicon  */}
+      <img className="h-[64px] w-[64px]" src="https://img.icons8.com/?size=100&id=2572&format=png&color=ffffffb3" alt="Logo Java" />
+      <img className="h-[64px] w-[64px]" src="https://img.icons8.com/?size=100&id=G0CnLqqcRBXl&format=png&color=ffffffb3" alt="Logo Java" />
       
+
+      {/* logotypes that don`t exist in simpleicon  */}
       </div>
   </Marquee>
 );

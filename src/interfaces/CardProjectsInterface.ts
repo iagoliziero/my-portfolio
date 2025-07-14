452355d6e6d0d4f971d4b5ec2b9513b977c6321d
iagoliziero <iagolizierox14@gmail.com>
2025-07-14 13:17:11 -0300

    feat: create CardProjectsInterface

---
+++ src/interfaces/CardProjectsInterface.ts
@@ -0,0 +1,12 @@
+export interface CardProjectsInterface {
+    id: number,
+    img: string,
+    title: string,
+    description: string,
+    techChallenge: string,
+    techStack: string[],
+    navigationGithub: string,
+    navigationDemo?: string,
+    navigateDemoExist: boolean,
+    
+}

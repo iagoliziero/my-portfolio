5941f593f6f533a5d6bdf39d27cadeabb8cc7e0d
iagoliziero <iagolizierox14@gmail.com>
2025-07-16 19:01:02 -0300

    refactor: change tag for logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,9 +5,11 @@ import UlContent from "./UlContent";
 const Header = (): JSX.Element => {
   return (
     <header className="fixed w-max-screen top-0 right-0 left-0 p-5 flex justify-between items-center blur-xs backdrop-blur-[0.5em] z-50 lg:justify-center lg:gap-60">
-      <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-500 font-bold text-2xl ">
+      <a
+      href="#"
+      className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-500 font-bold text-2xl ">
         &lt;IL Dev/&gt;
-      </span>
+      </a>
       <UlContent />
       <div className="flex flex-row gap-4 justify-center items-center">
         <Switch />

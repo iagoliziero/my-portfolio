4ef376b4c036f0885041f0e5050cc860f49710f0
iagoliziero <iagolizierox14@gmail.com>
2025-07-25 13:01:33 -0300

    refactor: adding more attributes from Input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,13 +1,15 @@
 interface Iinput {
     children: React.ReactNode,
-    placeholder: string
+    placeholder: string,
+    name: string,
+    type: string
 }
 
-const Input = ({children, placeholder}: Iinput): JSX.Element => {
+const Input = ({children, placeholder, name, type}: Iinput): JSX.Element => {
   return (
     <div className="flex flex-col gap-2 items-start">
         <label className="text-white/70"> {children} </label>
-        <input className="p-4 text-white bg-transparent rounded-xl w-full  border-slate-300/30 border focus:border-purple-400 transition-all ease-in-out duration-300 outline-none" required placeholder={placeholder} />
+        <input type={type} name={name} className="p-4 text-white bg-transparent rounded-xl w-full  border-slate-300/30 border focus:border-purple-400 transition-all ease-in-out duration-300 outline-none" required placeholder={placeholder} />
     </div>
   );
 }

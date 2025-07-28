4c7d26437fe5a02ac47d3a258dc1906a77ed17ba
iagoliziero <iagolizierox14@gmail.com>
2025-07-28 11:32:18 -0300

    feat: create Logo component and add in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,12 @@
 import Checkbox from "./CheckBox";
+import Logo from "./Logo";
 import Switch from "./Switch";
 import UlContent from "./UlContent";
 
 const Header = (): JSX.Element => {
   return (
     <header className="fixed w-max-screen top-0 right-0 left-0 p-5 flex justify-between items-center blur-xs backdrop-blur-[0.5em] z-50 lg:justify-center lg:gap-52">
-      <a
-      href="#"
-      className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-500 font-bold text-2xl ">
-        &lt;IL Dev/&gt;
-      </a>
+      <Logo />
       <UlContent />
       <div className="flex flex-row gap-4 justify-center items-center">
         <Switch />
---
+++ src/components/Logo.tsx
@@ -0,0 +1,11 @@
+const Logo = (): JSX.Element => {
+    return ( 
+        <a
+      href="#"
+      className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-500 font-bold text-2xl ">
+        &lt;IL Dev/&gt;
+      </a>
+     );
+}
+ 
+export default Logo;

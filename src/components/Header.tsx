2468c9aca7142f4bf38f3efb6c5f9e0fa01a5e79
iagoliziero <iagolizierox14@gmail.com>
2025-07-30 22:34:05 -0300

    feat: add theme toggle functionality and update styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,21 @@
 import Logo from "./Logo";
-import Switch from "./Switch";
 import UlContent from "./UlContent";
 import DropDownMenu from "./DropDownMenu";
+import { useTheme } from "../hooks/toogleTheme";
+import { Moon, Sun } from "lucide-react";
 
 const Header = (): JSX.Element => {
+
+  const {theme, toggleTheme} = useTheme();
 
   return (
     <header className="fixed w-max-screen top-0 right-0 left-0 p-5 flex justify-between items-center blur-xs backdrop-blur-[0.5em] z-50 lg:justify-center lg:gap-52">
       <Logo />
       <UlContent />
       <div className="flex flex-row gap-4 justify-center items-center">
-        <Switch />
+        <button onClick={toggleTheme}>
+          {theme === 'dark' ? <Sun size={30} className="text-white/70" /> : <Moon size={30} className="text-white/70" /> }
+        </button>
         <DropDownMenu />
       </div>
     </header>

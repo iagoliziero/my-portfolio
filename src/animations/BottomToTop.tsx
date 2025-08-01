b5828f7ea54a2ae01dafd17d2eddb3b93f0146c8
iagoliziero <iagolizierox14@gmail.com>
2025-07-31 22:35:10 -0300

    feat: create BottomToTop component

---
+++ src/animations/BottomToTop.tsx
@@ -0,0 +1,40 @@
+import {useLayoutEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+interface IBottomToTop {
+    children: React.ReactNode,
+}
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function BottomToTop({ children }: IBottomToTop) {
+  const sectionRef = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    const el = sectionRef.current;
+
+    gsap.fromTo(
+      el,
+      { opacity: 0, y: 500 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 1,
+        ease: 'power2.inOut',
+        scrollTrigger: {
+          trigger: el,
+          start: 'top 80%',
+          toggleActions: 'play none none reverse',
+        },
+      }
+    );
+
+  }, []);
+
+  return (
+    <div ref={sectionRef} className="w-full">
+      {children}
+    </div>
+  );
+}

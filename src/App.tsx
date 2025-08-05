5be2a5a4f128afccdf43858809d620031c2cd3a0
iagoliziero <iagolizierox14@gmail.com>
2025-08-04 21:06:57 -0300

    fix: adjust positioning in App

--- src/App.tsx
+++ src/App.tsx
@@ -12,15 +12,18 @@ import Resume from "./components/Resume";
 import GetContact from "./components/GetContact";
 import Footer from "./components/Footer";
 import BottomToTop from "./animations/BottomToTop";
+import LeftOrRight from "./animations/LeftOrRight";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900 dark:from-slate-900 dark:via-gray-950 dark:to-slate-800 transition-all duration-300 overflow-x-hidden scroll-smooth">
-      <div id="1" className="flex flex-col">
+      <div id="inicio" className="flex flex-col">
         <Header />
       </div>
+      <section className="flex flex-col gap-[25rem]">
         <BottomToTop>
-          {/* Hero Section */}
+         <div className="flex flex-col">
+           {/* Hero Section */}
           <section className="relative flex flex-col items-center justify-center top-60  xl:top-64 3xl:top-80">
             <HeroContent />
             <div className="flex flex-col items-center justify-center">
@@ -28,42 +31,44 @@ function App() {
               <Hero />
             </div>
           </section>
-        
-        {/* Hero Section */}
 
-        {/* Socials section */}
-        <section className="flex items-center justify-center gap-6 md:gap-7 relative bottom-10 top-48 xxl:top-52 3xl:top-64 ">
-          <SocialsSection />
-        </section>
-        {/* Socials section */}
-      
-      {/* arrow down */}
-      <div className="relative top-60 xl:top-64 3xl:top-80 text-white/70 left-1/2 transform -translate-x-1/2 animate-bounce">
-        <a href="#2">
-          <ChevronDown width={35} height={35} />
-        </a>
-      </div>
-      {/* arrow down */}
+          {/* Hero Section */}
 
-      </BottomToTop>
+          {/* Socials section */}
+          <section className="flex items-center justify-center gap-6 md:gap-7 relative bottom-10 top-48 xxl:top-52 3xl:top-64 ">
+            <SocialsSection />
+          </section>
+          {/* Socials section */}
 
-      <div className="max-h-0 relative top-[25rem] xl:top-[28rem] 3xl:top-[35rem] flex flex-col items-center ">
+          {/* arrow down */}
+          <div className="relative top-60 xl:top-64 3xl:top-80 text-white/70 left-1/2 transform -translate-x-1/2 animate-bounce">
+            <a href="#sobre">
+              <ChevronDown width={35} height={35} />
+            </a>
+          </div>
+          {/* arrow down */}
+         </div>
+        </BottomToTop>
+         <div className="flex flex-col items-center ">
         {/* About Me */}
-        <AboutMe />
+        <LeftOrRight position="right">
+          <AboutMe />
+        </LeftOrRight>
         {/* About Me */}
-        <span id="3"></span>
+        <span id="stack"></span>
         <TechStack />
         <TechStudy />
         {/* Projects Component */}
-        <span id="4"></span>
+        <span id="projetos"></span>
         <Projects />
         {/* Projects Component */}
-        <span id="5"></span>
+        <span id="curriculo"></span>
         <Resume />
-        <span id="6"></span>
+        <span id="contato"></span>
         <GetContact />
         <Footer />
       </div>
+      </section>
     </div>
   );
 }
